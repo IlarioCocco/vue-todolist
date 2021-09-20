ac5024dbe78ab29dea672e2b14e07c5339480e8c
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-20 18:09:09 +0200

    commit js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-const app = new Vue(
-    {
+const app = new Vue({
         el:"#root",
         data: {
             //array
             //img
             //ecc ecc
-
+            todos: [
+                "first step",
+                "second step",
+                "third step"
+            ],
+            newTodo: ''
         },
         methods: {
-            
+            aggiungiTodo: function() {
+                if (this.newTodo != "" ) {
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+            }
         }
+    }
 });
